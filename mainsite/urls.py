46eb84d654f43cfc4cee7374b02460496f3d4722
Instahyre <abhishek3yadav3@gmail.com>
2022-03-18 13:49:49 +0530

    Shop

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('shop/', views.shop, name='shop'),
+    path('insurance/', views.insurance, name='insurance'),
+    path('police/', views.police, name='police'),
+    path('repair/', views.repair, name='repair'),
 ]
--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def shop(request):
+    products = Product.objects.all()
+    return render(request, 'shop.html', {'products': products})
+
+def insurance(request):
+    insurances = Insurance.objects.all()
+    return render(request, 'insurance.html', {'insurances': insurances})
+
+def police(request):
+    return render(request, 'police.html')
+
+def repair(request):
+    repairs = Repair.objects.all()
+    return render(request, 'repair.html', {'repairs': repairs})
+    
