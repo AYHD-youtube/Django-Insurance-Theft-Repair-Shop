90bd66c03869f597ca127fd9009c91da665abae1
Instahyre <abhishek3yadav3@gmail.com>
2022-03-18 17:29:42 +0530

    Shop 2

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('insurance/', views.insurance, name='insurance'),
     path('police/', views.police, name='police'),
     path('repair/', views.repair, name='repair'),
+    path('shop/buy/', views.buy, name='buy'),
+    path('shop/buy/login/', views.buy_login, name='shop_buy_login'),
 ]
--- mainsite/views.py
+++ mainsite/views.py
@@ -1,5 +1,8 @@
+import email
 from django.shortcuts import render
 from .models import *
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate
 
 # Create your views here.
 def index(request):
@@ -19,4 +22,34 @@ def police(request):
 def repair(request):
     repairs = Repair.objects.all()
     return render(request, 'repair.html', {'repairs': repairs})
-    
+
+@csrf_exempt
+def buy(request):
+    if request.method == 'POST':
+        product_id = request.POST.get('product_id')
+        product = Product.objects.get(id=product_id)
+        return render(request, 'buy.html', {'product': product})
+    else:
+        return render(request, 'shop.html')
+
+@csrf_exempt
+def buy_login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = User.objects.filter(email=email)
+        product = Product.objects.get(id=request.POST.get('product_id'))
+        if not user:     
+            user = User.objects.create(email=email, password=password, is_customer=True, username=email)
+        else:
+            user = authenticate(username=email, password=password)
+            if user is not None:
+                if not user.is_customer:
+                    return render(request, 'buy.html', {'error': 'You are not a customer!', 'product': product})
+                else:
+                    return render(request, 'reciept.html', {'user': user, 'product': product})
+            else:
+                return render(request, 'buy.html', {'error': 'Wrong email or password!', 'product': product})
+    else:
+        return render(request, 'buy.html', {'error': 'Wrong email or password!', 'product': product})
+        
